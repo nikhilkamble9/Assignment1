f17b10285861140c714a80e1f89168db7ddb36a6
Nikhil Kamble <98752032+nikhilkamble9@users.noreply.github.com>
2022-02-01 16:43:40 +0530

    Add files via upload

--- Assignment1/src/Assignment-3/assignment3_test.js
+++ Assignment1/src/Assignment-3/assignment3_test.js
@@ -14,20 +14,19 @@
 	var quixote = require("../../vendor/quixote.js");
   
 	// Define a test suite using Mocha's standard `define` function.
-	describe("Assignment-5", function () {
+	describe("Assignment-3", function () {
 	  // Variables used by our tests. They're populated in the `before()` and `beforeEach()` functions.
 	  var frame; // The Quixote test frame.
 	  var container, frameDom; // The figure element inside the media object. (The icon.)
-	  var header_ele, form_ele, input; // The content element inside the media object. (The paragraph.)
   
 	  // Use Mocha's standard `before` function to set up our Quixote test frame before the tests run.
 	  before(function (done) {
 		// Create the frame and load our stylesheet.
 		frame = quixote.createFrame(
 		  {
-			src: "/base/src/Assignment-5/assignment5.html", // the server under test must be proxied to localhost
+			src: "/base/src/Assignment-3/assignment3.html", // the server under test must be proxied to localhost
 			// The URL of our stylesheet. It's served by Karma and configured in `build/config/karma.conf.js`.
-			stylesheet: "/base/src/Assignment-5/assignment5.css",
+			stylesheet: "/base/src/Assignment-3/assignment3.css",
 		  },
 		  done
 		); // This is an asynchronous operation, so we pass in Mocha's `done` callback.
@@ -50,29 +49,42 @@
 		// tests don't affect other tests. We do it in the `beforeEach()` function rather than `afterEach()`
 		// to make debugging easier.
 		frame.reset();
+		frameDom = frame.toDomElement().contentDocument;
+		container = frame.get("#container");
+  
 		// Get the media element's figure and content elements. Quixote gives us an object we can use to make
 		// assertions about how the elements are styled.
 	  });
   
-	  
-	  it("Page should have two cards with class name- 'card'", function () {
-			assert.isTrue(
-			  frame.toDomElement().contentDocument.getElementsByClassName("card")
-				.length === 2
-			);
-		  });
-		  it("Page should have two image tags", function () {
-			assert.isTrue(
-			  frame.toDomElement().contentDocument.getElementsByTagName("img")
-				.length === 2
-			);
-		  });
-		  it("Card should have flex property", function () {
-			assert.isTrue(window.getComputedStyle(frame
-				.toDomElement()
-				.contentDocument.getElementsByClassName('card')[0]).getPropertyValue('display')==="flex");
-		  });
-  
+	  // Our first test. We use Mocha's standard `it()` function to define the test. Here, we're checking that
+
+	  it("container should have iframe for page", function () {
+		assert.isTrue(
+			frame.toDomElement().contentDocument.getElementsByTagName("iframe")
+			  .length === 2
+		  );
+	  });
+	  it("Page should have two Heading tags", function () {
+		assert.isTrue(
+		  frame.toDomElement().contentDocument.getElementsByTagName("h1")
+			.length === 2
+		);
+	  });
+	  it("Page should have two Paragraph tags'", function () {
+		assert.isTrue(
+		  frame.toDomElement().contentDocument.getElementsByTagName("p")
+			.length === 2
+		);
+	  });
+	  it("Page should have two Button tags", function () {
+		assert.isTrue(
+		  frame.toDomElement().contentDocument.getElementsByTagName("button")
+			.length === 2
+		);
+	  });
 	});
+	function isExist(target) {
+	  return target.length > 0 ? true : false;
+	}
   })();
   
